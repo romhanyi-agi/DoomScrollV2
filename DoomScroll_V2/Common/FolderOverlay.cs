38c7fd5f7694af5b199ce4f143ec60f9f0a88862
Romhányi Ágnes <romhanyi.agi@gmail.com>
2023-02-26 20:55:39 -0800

    UI overlays

---
+++ DoomScroll_V2/Common/FolderOverlay.cs
@@ -0,0 +1,81 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+namespace DoomScroll_V2.Common
+{
+    //a static container for a set of methods operating on input parameters without having to get or set any internal instance fields.
+    public static class FolderOverlay
+    {
+        private static Vector2 buttonSize = new Vector2(0.5f, 0.5f);
+        public static CustomButton CreateFolderBtn(GameObject parent)
+        {
+            GameObject keyboardBtn = HudManager.Instance.Chat.OpenKeyboardButton;
+            Vector3 pos = keyboardBtn.transform.localPosition;
+            SpriteRenderer sr = keyboardBtn.GetComponent<SpriteRenderer>();
+            Vector2 size = sr ? sr.size : new Vector2(0.5f, 0.5f);
+            Vector3 position = new(pos.x, pos.y + size.y + 0.1f, pos.z);
+            Vector4[] slices = { new Vector4(0, 0.5f, 1, 1), new Vector4(0, 0, 1, 0.5f) };
+            Sprite[] btnSprites = ImageLoader.ReadImageSlicesFromAssembly(Assembly.GetExecutingAssembly(), "DoomScroll_V2.Assets.folderToggle.png", slices);
+            CustomButton folderBtn = new CustomButton(parent, btnSprites, position, size.x, "FolderToggleButton");
+            folderBtn.ActivateButton(false);
+            return folderBtn;
+        }
+
+        public static GameObject CreateFolderOverlay(GameObject parent)
+        {
+            SpriteRenderer backgroundSR = parent.transform.Find("Background").GetComponent<SpriteRenderer>();
+            // create the overlay background
+            GameObject folderOverlay = new GameObject("FolderOverlay");
+            folderOverlay.layer = LayerMask.NameToLayer("UI");
+            folderOverlay.transform.SetParent(parent.transform);
+            folderOverlay.transform.localPosition = new Vector3(0f, 0f, -10f);
+            SpriteRenderer sr = folderOverlay.AddComponent<SpriteRenderer>();
+            Sprite spr = ImageLoader.ReadImageFromAssembly(Assembly.GetExecutingAssembly(), "DoomScroll_V2.Assets.folderOverlay.png");
+            sr.sprite = spr;
+            if (backgroundSR != null)
+            {
+                sr.drawMode = SpriteDrawMode.Sliced;
+                sr.size = backgroundSR.size; // the size to render works only when the drawMode is set to SpriteDrawMode.Sliced. !!!!
+            }
+            else
+            {
+                folderOverlay.transform.localScale = parent.transform.localScale * 0.4f;
+            }
+            return folderOverlay;
+        }
+
+        public static CustomButton AddCloseButton(GameObject parent)
+        {
+            SpriteRenderer sr = parent.GetComponent<SpriteRenderer>();
+            Vector3 position = new Vector3(-sr.size.x / 2 - buttonSize.x / 2, sr.size.y / 2 - buttonSize.y / 2, -5f);
+            Sprite[] closeBtnImg = { ImageLoader.ReadImageFromAssembly(Assembly.GetExecutingAssembly(), "DoomScroll_V2.Assets.closeButton.png") };
+            return new CustomButton(parent, closeBtnImg, position, buttonSize.x, "Close FolderOverlay");
+        }
+        public static CustomButton AddHomeButton(GameObject parent)
+        {
+            SpriteRenderer sr = parent.GetComponent<SpriteRenderer>();
+            Vector4[] slices = { new Vector4(0, 0.5f, 1, 1), new Vector4(0, 0, 1, 0.5f) };
+            Sprite[] homeBtnImg = ImageLoader.ReadImageSlicesFromAssembly(Assembly.GetExecutingAssembly(), "DoomScroll_V2.Assets.homeButton.png", slices);
+            Vector3 homePosition = new Vector3(-sr.size.x / 2 + buttonSize.x + 0.2f, sr.size.y / 2 - buttonSize.y, -5f);
+            return new CustomButton(parent, homeBtnImg, homePosition, buttonSize.x, "Back to Home");
+
+        }
+        public static CustomButton AddBackButton(GameObject parent)
+        {
+            SpriteRenderer sr = parent.GetComponent<SpriteRenderer>();
+            Vector4[] slices = { new Vector4(0, 0.5f, 1, 1), new Vector4(0, 0, 1, 0.5f) };
+            Sprite[] backBtnImg = ImageLoader.ReadImageSlicesFromAssembly(Assembly.GetExecutingAssembly(), "DoomScroll_V2.Assets.backButton.png", slices);
+            Vector3 backPosition = new Vector3(-sr.size.x / 2 + 2 * buttonSize.x + 0.2f, sr.size.y / 2 - buttonSize.y, -5f);
+            return new CustomButton(parent, backBtnImg, backPosition, buttonSize.x, "Back to Previous");
+
+        }
+        public static CustomText AddPath(GameObject parent)
+        {
+            SpriteRenderer sr = parent.GetComponent<SpriteRenderer>();
+            CustomText pathText = new CustomText("PathName", parent, "Home");
+            // RectTransform rt = pathText.AddComponent<RectTransform>();
+            // rt.sizeDelta = new Vector2(-sr.size.x / 2 - 3 * buttonSize.x + 0.2f, sr.size.y / 2 - buttonSize.y);
+            return pathText;
+        }
+    }
+}
---
+++ DoomScroll_V2/Common/ScreenshotOverlay.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+namespace DoomScroll_V2.Common
+{
+    //a static container for a set of methods operating on input parameters without having to get or set any internal instance fields.
+    internal class ScreenshotOverlay
+    {
+        public static CustomButton CreateCameraButton(HudManager hud)
+        {
+            GameObject m_UIParent = hud.gameObject;
+            Vector3 mapBtnPos = hud.MapButton.transform.position;
+            SpriteRenderer mapButtonSr = hud.MapButton.GetComponent<SpriteRenderer>();
+            Vector3 position = new Vector3(mapBtnPos.x, mapBtnPos.y - mapButtonSr.size.y * hud.MapButton.transform.localScale.y, mapBtnPos.z);
+            Vector2 scaledSize = mapButtonSr.size * hud.MapButton.transform.localScale;
+            Vector4[] slices = { new Vector4(0, 0.5f, 1, 1), new Vector4(0, 0, 1, 0.5f) };
+            Sprite[] cameraBtnSprites = ImageLoader.ReadImageSlicesFromAssembly(Assembly.GetExecutingAssembly(), "DoomScroll_V2.Assets.cameraFlash.png", slices);
+
+            return new CustomButton(m_UIParent, cameraBtnSprites, position, scaledSize.x, "Camera Toggle Button");
+        }
+
+        public static CustomButton CreateCaptureButton(GameObject parent)
+        {
+            Vector4[] slices = { new Vector4(0, 0.5f, 1, 1), new Vector4(0, 0, 1, 0.5f) };
+            Sprite[] captureSprite = ImageLoader.ReadImageSlicesFromAssembly(Assembly.GetExecutingAssembly(), "DoomScroll_V2.Assets.captureScreenNew.png", slices);
+            SpriteRenderer sr = parent.GetComponent<SpriteRenderer>();
+            Vector3 pos = new Vector3(sr.size.x / 2 - 0.7f, 0, -10);
+
+            return new CustomButton(parent, captureSprite, pos, 0.6f, "Screenshot Button");
+        }
+
+        public static GameObject InitCameraOverlay(HudManager hud)
+        {
+            GameObject cameraOverlay = new GameObject("ScreenshotOverlay");
+            cameraOverlay.layer = LayerMask.NameToLayer("UI");
+            cameraOverlay.transform.SetParent(hud.transform);
+            cameraOverlay.transform.localPosition = new Vector3(0f, 0f, -5);
+
+            SpriteRenderer sr = cameraOverlay.AddComponent<SpriteRenderer>();
+            Sprite spr = ImageLoader.ReadImageFromAssembly(Assembly.GetExecutingAssembly(), "DoomScroll_V2.Assets.cameraOverlay.png");
+            sr.sprite = spr;
+
+            // deactivate by default
+            cameraOverlay.SetActive(false);
+            return cameraOverlay;
+        }
+    }
+}
