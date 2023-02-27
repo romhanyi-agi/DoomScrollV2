22bfe33f612e65f693cba2b9a7868a72e99bb782
Romhányi Ágnes <romhanyi.agi@gmail.com>
2023-02-26 20:53:31 -0800

    Events

---
+++ DoomScroll_V2/Common/DoomScrollEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoomScroll_V2.Common
+{
+    public class DoomScrollEvent
+    {
+        public event Action MyAction;
+        public void InvokeAction()
+        {
+            MyAction?.Invoke();
+        }
+    }
+}
