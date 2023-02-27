0d97305dcd59b226e6b620cdec321a10a8d7af10
Romhányi Ágnes <romhanyi.agi@gmail.com>
2023-02-26 20:54:13 -0800

    Directory management

---
+++ DoomScroll_V2/Common/IDirectory.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace DoomScroll_V2.Common
+{
+    public interface IDirectory
+    {
+        public GameObject Dir { get; }
+        public CustomButton Btn { get; }
+        public CustomText Label { get; }
+        public void DisplayContent();
+        public string PrintDirectory();
+    }
+}
